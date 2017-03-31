edca4f574094e696f40ddeeac876fd25ddd51bee
duanwalker <theoriginalcpututor@gmail.com>
2017-03-30 23:26:44 -0400

    adding slick image carousel

--- js/grayscale_custom.js
+++ js/grayscale_custom.js
@@ -1,9 +1,20 @@
+/* Built with <3 by Duan Walker 2017 */
+
 $(document).ready(function(){
 
-$(".btn1").click(function(){
-	$(".intro").fadeOut(30000);
-	Console.log("btn1 was clicked")
-})
+//load gallary with images 
+
+
+//slick image carousel
+$('.fade').slick({
+  dots: true,
+  infinite: true,
+  speed: 600,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 3500,
+});
 	
 
 })
